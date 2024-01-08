6afb2adbd21f8a7477f70c2a2afcf5f66c272330
Tran Xuan Khoi <khoitran7122002@gmail.com>
2024-01-08 14:23:03 +0700

    Match animation to movement

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -3,16 +3,22 @@ using UnityEngine.AI;
 
 public class Mover : MonoBehaviour {
 
+    private const string FORWARDSPEED = "forwardSpeed";
+
     private NavMeshAgent navMeshAgent;
+    private Animator animator;
 
     private void Awake() {
         navMeshAgent = GetComponent<NavMeshAgent>();
+        animator = GetComponent<Animator>();
     }
 
     private void Update() {
         if (Input.GetMouseButtonDown(1)) {
             MoveToCursor();
         }
+
+        UpdateAnimator();
     }
 
     private void MoveToCursor() {
@@ -23,6 +29,16 @@ public class Mover : MonoBehaviour {
         if (hasHit) {
             navMeshAgent.destination = hit.point;
         }
+    }
+
+    private void UpdateAnimator() {
+        Vector3 velocity = navMeshAgent.velocity;
+        Vector3 localVelocity = transform.InverseTransformDirection(velocity);
+        float speed = localVelocity.z;
+
+        animator.SetFloat(FORWARDSPEED, speed);
+
+        Debug.Log($"Velocity: {velocity} - Local velocity: {localVelocity}");
     }
 
 }
