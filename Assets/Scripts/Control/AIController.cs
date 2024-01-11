e61ae4a8566337a6bec98caae29ecc12e64aa53f
Tran Xuan Khoi <khoitran7122002@gmail.com>
2024-01-11 14:08:47 +0700

    Visuallizing with Gizmos

--- Assets/Scripts/Control/AIController.cs
+++ Assets/Scripts/Control/AIController.cs
@@ -33,5 +33,10 @@ namespace RPG.Control {
             float distanceToPlayer = Vector3.Distance(transform.position, player.transform.position);
             return distanceToPlayer < chaseDistance;
         }
+
+        private void OnDrawGizmosSelected() {
+            Gizmos.color = Color.red;
+            Gizmos.DrawWireSphere(transform.position, chaseDistance);
+        }
     }
 }
