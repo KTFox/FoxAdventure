8d8a380b1e2c9396311efe8dc3f2df54166ce449
Tran Xuan Khoi <khoitran7122002@gmail.com>
2024-01-17 16:06:17 +0700

    Add pickup respawning behaviour

--- Assets/Scripts/Combat/WeaponPickup.cs
+++ Assets/Scripts/Combat/WeaponPickup.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections;
 using UnityEngine;
 
 namespace RPG.Combat {
@@ -5,12 +7,37 @@ namespace RPG.Combat {
 
         [SerializeField]
         private WeaponSO weaponSO;
+        [SerializeField]
+        private float hideTime;
 
         private void OnTriggerEnter(Collider collision) {
             if (collision.CompareTag("Player")) {
                 collision.GetComponent<Fighter>().EquipWeapon(weaponSO);
+                StartCoroutine(HideForSeconds(hideTime));
+            }
+        }
 
-                Destroy(gameObject);
+        private IEnumerator HideForSeconds(float seconds) {
+            HidePickup();
+
+            yield return new WaitForSeconds(seconds);
+
+            ShowPickup();
+        }
+
+        private void HidePickup() {
+            GetComponent<SphereCollider>().enabled = false;
+
+            foreach (Transform child in transform) {
+                child.gameObject.SetActive(false);
+            }
+        }
+
+        private void ShowPickup() {
+            GetComponent<SphereCollider>().enabled = true;
+
+            foreach (Transform child in transform) {
+                child.gameObject.SetActive(true);
             }
         }
     }
