5e3e60a65f5d99ce6b098db42aee32b768675495
Tran Xuan Khoi <khoitran7122002@gmail.com>
2024-01-17 11:23:02 +0700

    Improve projecttitle behaviour

--- Assets/Scripts/Combat/Projectile.cs
+++ Assets/Scripts/Combat/Projectile.cs
@@ -6,22 +6,33 @@ namespace RPG.Combat {
 
         [SerializeField]
         private float flyingSpeed;
+        [SerializeField]
+        private bool isChasingProjectTile = true;
         private float damage;
 
         private Health target;
 
+        private void Start() {
+            transform.LookAt(GetAimLocation());
+        }
+
         private void Update() {
             if (target == null) return;
 
-            transform.LookAt(GetAimLocation());
+            if (isChasingProjectTile && !target.IsDeath()) {
+                transform.LookAt(GetAimLocation());
+            }
+
             transform.Translate(Vector3.forward * flyingSpeed * Time.deltaTime);
         }
 
         private void OnTriggerEnter(Collider collision) {
             if (collision.GetComponent<Health>() != target) return;
+            if (target.IsDeath()) return;
 
             collision.GetComponent<Health>().TakeDamage(damage);
-            Destroy(gameObject);    
+
+            Destroy(gameObject);
         }
 
         private Vector3 GetAimLocation() {
