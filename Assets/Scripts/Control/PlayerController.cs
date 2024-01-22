1c9b3b53c9b19d336f716a2b51eb2deb7bbf70f7
Tran Xuan Khoi <khoitran7122002@gmail.com>
2024-01-22 11:23:02 +0700

    Calculate NavMeshPath length

--- Assets/Scripts/Control/PlayerController.cs
+++ Assets/Scripts/Control/PlayerController.cs
@@ -19,6 +19,8 @@ namespace RPG.Control {
         private CursorMapping[] cursorMappings;
         [SerializeField]
         private float maxNavMeshProjectionDistance = 1f;
+        [SerializeField]
+        private float maxNavPathLength;
 
         private Mover mover;
         private Health health;
@@ -103,7 +105,27 @@ namespace RPG.Control {
             if (!hasCastToNavMesh) return false;
 
             target = navMeshHit.position;
+
+            NavMeshPath path = new NavMeshPath();
+            bool hasPath = NavMesh.CalculatePath(transform.position, target, NavMesh.AllAreas, path);
+            if (!hasPath) return false;
+            if (path.status != NavMeshPathStatus.PathComplete) return false;
+            if (GetNavPathLength(path) > maxNavPathLength) return false;
+
             return true;
+        }
+
+        private float GetNavPathLength(NavMeshPath path) {
+            float pathLength = 0f;
+
+            if (path.corners.Length < 2) return pathLength;
+
+            for (int i = 0; i < path.corners.Length - 1; i++) {
+                float distance = Vector3.Distance(path.corners[i], path.corners[i + 1]);
+                pathLength += distance;
+            }
+
+            return pathLength;
         }
 
         private void SetCursor(CursorType type) {
