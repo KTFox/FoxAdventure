f0935f6cd39c813c6c85d74ac04a05e2885bb6a3
Tran Xuan Khoi <khoitran7122002@gmail.com>
2024-01-29 16:24:08 +0700

    Reorganise project folder

---
+++ Assets/_Scripts/Control/PatrolPath.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+namespace RPG.Control {
+    public class PatrolPath : MonoBehaviour {
+
+        private float waypointGizmosRadius;
+
+        private void OnDrawGizmos() {
+            for (int i = 0; i < transform.childCount; i++) {
+                Gizmos.DrawSphere(GetWaypointPosition(i), waypointGizmosRadius);
+                Gizmos.DrawLine(GetWaypointPosition(i), GetWaypointPosition(GetNextIndex(i)));
+            }
+        }
+
+        public int GetNextIndex(int index) {
+            if (index == transform.childCount - 1) {
+                return 0;
+            } else {
+                return index + 1;
+            }
+        }
+
+        public Vector3 GetWaypointPosition(int index) {
+            return transform.GetChild(index).position;
+        }
+
+        public int GetWaypointCount() {
+            return transform.childCount;    
+        }
+    }
+}
